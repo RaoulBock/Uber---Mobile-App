8b9a64a41be4cefb92d37ae837668195ffe7f88d
RaoulBock <anthonyraoulbock01@gmail.com>
2023-01-23 12:57:54 +0200

    front-end

--- Components/List/SpecList.js
+++ Components/List/SpecList.js
@@ -5,7 +5,7 @@ import { APP_ICON } from "../../Context/settings";
 const SpecList = ({ item }) => {
   return (
     <View style={styles.outline}>
-      <View style={styles.grid}>
+      <View style={[styles.grid, { justifyContent: "space-between" }]}>
         <Image source={{ uri: item.img }} style={styles.img} />
         <View style={styles.grid}>
           <TouchableOpacity style={{ marginHorizontal: 20 }}>
@@ -14,6 +14,48 @@ const SpecList = ({ item }) => {
           <TouchableOpacity>
             <Text>{APP_ICON.CALL}</Text>
           </TouchableOpacity>
+        </View>
+      </View>
+      <View
+        style={[
+          styles.grid,
+          {
+            alignItems: "center",
+            borderBottomWidth: 1,
+            borderBottomColor: "#eee",
+            paddingVertical: 10,
+            justifyContent: "space-between",
+          },
+        ]}
+      >
+        <View>
+          <Text style={styles.title}>{item.name} is on the way</Text>
+          <Text>12:43 PM Drop-Off</Text>
+        </View>
+        <View>
+          <Text style={styles.title}>{item.plate_no}</Text>
+          <Text style={styles.text}>License Plate No.</Text>
+        </View>
+      </View>
+
+      <View style={[{}]}>
+        <View
+          style={[styles.grid, { alignItems: "center", marginVertical: 20 }]}
+        >
+          <Text>{APP_ICON.LOCATION}</Text>
+          <View style={{ paddingHorizontal: 10 }}>
+            <Text style={styles.text}>Start Location</Text>
+            <Text style={styles.location}>
+              B Street 920025, CA - 3:00 to 3:15 PM
+            </Text>
+          </View>
+        </View>
+        <View style={[styles.grid, { alignItems: "center" }]}>
+          <Text>{APP_ICON.LOCATION}</Text>
+          <View style={{ paddingHorizontal: 10 }}>
+            <Text style={styles.text}>Dropoff</Text>
+            <Text style={styles.location}>SJC, Terminal B</Text>
+          </View>
         </View>
       </View>
     </View>
@@ -30,6 +72,21 @@ const styles = StyleSheet.create({
   },
   grid: {
     flexDirection: "row",
-    justifyContent: "space-between",
+  },
+  title: {
+    color: "#404040",
+    fontWeight: "500",
+    fontSize: 25,
+    marginTop: 10,
+  },
+  text: {
+    color: "gray",
+    fontWeight: "500",
+    fontSize: 12,
+  },
+  location: {
+    color: "#404040",
+    fontWeight: "500",
+    fontSize: 20,
   },
 });
